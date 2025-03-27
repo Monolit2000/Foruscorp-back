190dbd7a7025d24e637fff6c7418fee073423678
Andrii <kostuchenkojr@gamil.com>
2025-03-27 21:10:56 +0200

    commit

--- src/TrucksTracking/Foruscorp.TrucksTracking.API/Program.cs
+++ src/TrucksTracking/Foruscorp.TrucksTracking.API/Program.cs
@@ -29,7 +29,7 @@ if (app.Environment.IsDevelopment())
 
 app.UseCors("AllowAll");
 
-app.MapHub<TruckHub>("/truck-trañking");
+app.MapHub<TruckHub>("/truck-tracking");
 
 //app.UseHttpsRedirection();
 
