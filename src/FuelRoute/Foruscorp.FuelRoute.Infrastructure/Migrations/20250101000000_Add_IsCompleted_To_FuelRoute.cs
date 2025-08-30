6b05dd1ebc7c74f49147cb09ee92520b87629b26
Andrii <kostuchenkojr@gmail.com>
2025-08-30 03:57:55 +0300

    add prod docker

--- src/FuelRoute/Foruscorp.FuelRoute.Infrastructure/Migrations/20250101000000_Add_IsCompleted_To_FuelRoute.cs
+++ src/FuelRoute/Foruscorp.FuelRoute.Infrastructure/Migrations/20250101000000_Add_IsCompleted_To_FuelRoute.cs
@@ -10,22 +10,14 @@ namespace Foruscorp.FuelRoutes.Infrastructure.Migrations
         /// <inheritdoc />
         protected override void Up(MigrationBuilder migrationBuilder)
         {
-            migrationBuilder.AddColumn<bool>(
-                name: "IsCompleted",
-                schema: "FuelRoutes",
-                table: "FuelRoutes",
-                type: "boolean",
-                nullable: false,
-                defaultValue: false);
+
         }
 
         /// <inheritdoc />
         protected override void Down(MigrationBuilder migrationBuilder)
         {
-            migrationBuilder.DropColumn(
-                name: "IsCompleted",
-                schema: "FuelRoutes",
-                table: "FuelRoutes");
+
         }
     }
 }
+
